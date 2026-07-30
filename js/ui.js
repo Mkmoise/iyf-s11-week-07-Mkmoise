5e4682b9de5c4375f4a72605e2409bbb11ba2f2d
Mkmoise <mkmoise920@gmail.com>
2026-07-30 12:38:35 +0300

    update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,9 @@
-licationState.taskCollection.forEach(function (taskItem) {
+function displayTaskCollection() {
+    const taskItemsContainer = document.getElementById("task-items-container");
+
+    taskItemsContainer.innerHTML = "";
+
+    applicationState.taskCollection.forEach(function (taskItem) {
         const taskListItem = document.createElement("li");
 
         taskListItem.textContent = taskItem.taskDescription;
